b1a9342423f57ed28200c2afc8d4ec7195ab8e3a
shaikhasif69 <theshaikhasif03@gmail.com>
2024-09-21 06:12:28 +0530

    sai

--- lib/screens/Staffs/staff_profile_page.dart
+++ lib/screens/Staffs/staff_profile_page.dart
@@ -1,15 +1,234 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:pillai_hackcelestial/components/constant.dart';
 
 class StaffProfilePage extends StatefulWidget {
-  const StaffProfilePage({super.key});
+  const StaffProfilePage({Key? key}) : super(key: key);
 
   @override
-  State<StaffProfilePage> createState() => _StaffProfilePageState();
+  State<StaffProfilePage> createState() => _DoctorProfilePageState();
 }
 
-class _StaffProfilePageState extends State<StaffProfilePage> {
+class _DoctorProfilePageState extends State<StaffProfilePage> {
+  late String userName = "";
+
+  @override
+  void initState() {
+    super.initState();
+    // getUserDetails();
+  }
+
+  // Future<void> getUserDetails() async {
+  //   final hiveService = HiveService();
+  //   DoctorHive? doctor = await hiveService.getDoctor();
+
+  //   if (doctor != null) {
+  //     print('doctor ID: ${doctor.id}');
+  //     print('doctor Name: ${doctor.firstName} ${doctor.lastName}');
+  //     setState(() {
+  //       userName = '${doctor.firstName} ${doctor.lastName}';
+  //     });
+  //     // Use the user data as needed
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: MyColors.backgroundColorLight,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: MyColors.backgroundColorLight,
+        foregroundColor: Colors.black,
+        title: const Text("PROFILE"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings_rounded),
+          )
+        ],
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(10),
+        children: [
+          // COLUMN THAT WILL CONTAIN THE PROFILE
+          Column(
+            children:  [
+              const CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage(
+                  "lib/icons/theSergeon.jpg",
+                ),
+              ),
+             const SizedBox(height: 10),
+              Text(
+                userName,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text("Senior Surgeon")
+            ],
+          ),
+          const SizedBox(height: 25),
+          Row(
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(right: 5),
+                child: Text(
+                  "Complete your profile",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                "(1/5)",
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              )
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            children: List.generate(5, (index) {
+              return Expanded(
+                child: Container(
+                  height: 7,
+                  margin: EdgeInsets.only(right: index == 4 ? 0 : 6),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: index == 0 ? Colors.blue : Colors.black12,
+                  ),
+                ),
+              );
+            }),
+          ),
+          const SizedBox(height: 10),
+          SizedBox(
+            height: 180,
+            child: ListView.separated(
+              physics: const BouncingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                final card = profileCompletionCards[index];
+                return SizedBox(
+                  width: 160,
+                  child: Card(
+                    shadowColor: Colors.black12,
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        children: [
+                          Icon(
+                            card.icon,
+                            size: 30,
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            card.title,
+                            textAlign: TextAlign.center,
+                          ),
+                          const Spacer(),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                            child: Text(card.buttonText),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) =>
+                  const Padding(padding: EdgeInsets.only(right: 5)),
+              itemCount: profileCompletionCards.length,
+            ),
+          ),
+          const SizedBox(height: 35),
+          ...List.generate(
+            customListTiles.length,
+            (index) {
+              final tile = customListTiles[index];
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 5),
+                child: Card(
+                  elevation: 4,
+                  shadowColor: Colors.black12,
+                  child: ListTile(
+                    leading: Icon(tile.icon),
+                    title: Text(tile.title),
+                    trailing: const Icon(Icons.chevron_right),
+                  ),
+                ),
+              );
+            },
+          )
+        ],
+      ),
+    );
   }
 }
+
+class ProfileCompletionCard {
+  final String title;
+  final String buttonText;
+  final IconData icon;
+  ProfileCompletionCard({
+    required this.title,
+    required this.buttonText,
+    required this.icon,
+  });
+}
+
+List<ProfileCompletionCard> profileCompletionCards = [
+  ProfileCompletionCard(
+    title: "Set Your Profile Details",
+    icon: CupertinoIcons.person_circle,
+    buttonText: "Continue",
+  ),
+  ProfileCompletionCard(
+    title: "Upload your Documents",
+    icon: CupertinoIcons.doc,
+    buttonText: "Upload",
+  ),
+  ProfileCompletionCard(
+    title: "Add your Expertise",
+    icon: CupertinoIcons.square_list,
+    buttonText: "Add",
+  ),
+];
+
+class CustomListTile {
+  final IconData icon;
+  final String title;
+  CustomListTile({
+    required this.icon,
+    required this.title,
+  });
+}
+
+List<CustomListTile> customListTiles = [
+  CustomListTile(
+    icon: Icons.insights,
+    title: "Activity",
+  ),
+  CustomListTile(
+    title: "Notifications",
+    icon: CupertinoIcons.bell,
+  ),
+  CustomListTile(
+    title: "Logout",
+    icon: CupertinoIcons.arrow_right_arrow_left,
+  ),
+];
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import file_selector_macos
 import path_provider_foundation
 import rive_common
 import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
